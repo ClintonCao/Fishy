6d39152e44244bed1077809bb6db11257ee4cea0
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2015-09-01 21:09:13 +0200

    Added getters and setters to entity and sprite classes.

--- src/main/Entity.java
+++ src/main/Entity.java
@@ -1,17 +1,43 @@
 package main;
 
+/**
+ * An entity represents an entity in the game world. It has a movement speed, an alive state, and
+ * a sprite as a model.
+ * @author Clinton Cao, Dmity Malarev, Matthijs Halvemaan, Sunwei Wang, Michiel Doesburg.
+ *
+ */
 public class Entity {
 	private double movespeed;
 	private boolean isAlive;
 	private Sprite sprite;
 	
 	public Entity(double movespeed, boolean isAlive, Sprite sprite) {
-		this.movespeed = movespeed;
-		this.isAlive = isAlive;
+		this.setMovespeed(movespeed);
+		this.setAlive(isAlive);
 		this.sprite = sprite;
 	}
 	
 	public Sprite getSprite() {
 		return this.sprite;
+	}
+	
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+
+	public double getMovespeed() {
+		return movespeed;
+	}
+
+	public void setMovespeed(double movespeed) {
+		this.movespeed = movespeed;
 	}
 }
--- src/main/Sprite.java
+++ src/main/Sprite.java
@@ -2,9 +2,9 @@ package main;
 import javafx.scene.image.Image;
 
 /**
- *
+ * A sprite represents the model of an entity on the screen. It has a position in X and Y coordinates,
+ * a certain velocity in a direction expressed by X and Y coordinates, A width and a height, and an image to draw on the screen.
  * @author Clinton Cao, Dmity Malarev, Matthijs Halvemaan, Sunwei Wang, Michiel Doesburg.
- * 
  */
 public class Sprite {
 	private Image img;
@@ -17,15 +17,69 @@ public class Sprite {
 	
 	public Sprite(Image img, double posX, double posY, double velX, double velY, double width, double height) {
 		this.img = img;
-		this.posX = posX;
-		this.posY = posY;
-		this.velX = velX;
-		this.velY = velY;
-		this.width = width;
-		this.height = height;
+		this.setPosX(posX);
+		this.setPosY(posY);
+		this.setVelX(velX);
+		this.setVelY(velY);
+		this.setWidth(width);
+		this.setHeight(height);
 	}
 	
+	
+//------------Getters and setters-------------------------------------------
 	public Image getImg() {
 		return this.img;
+	}
+	
+	public void setImg(Image img) {
+		this.img = img;
+	}
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public double getVelY() {
+		return velY;
+	}
+
+	public void setVelY(double velY) {
+		this.velY = velY;
+	}
+
+	public double getVelX() {
+		return velX;
+	}
+
+	public void setVelX(double velX) {
+		this.velX = velX;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
 	}
 }
