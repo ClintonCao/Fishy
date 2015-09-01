75583c6067d56e517a78838111bdb12f9c98f3d6
Clinton Cao <C.S.Cao@student.tudelft.nl>
2015-09-01 20:01:49 +0200

    Added scene and event handlers.
    I implemented the basic part of the GUI. You can run Game.java to start
    the game. Once it starts, you will see the Main Screen. If you press any
    of the buttons, a line will be printed on the console. This is for testing
    purposes only. I will work on the rest.

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,16 +1,74 @@
 package main;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
+	static Stage stage;
+	static Pane pane;
+//	final URL resource = getClass().getResource("FXML/track.mp3");
+//    final Media media = new Media(resource.toString());
+//    static MediaPlayer mediaPlayer = null;
+    
+    
+    
+    
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(Game.class, (java.lang.String[]) null);
 	}
-	
-	public void start(Stage theStage) {
-		theStage.setTitle("Fishy");
-		theStage.show();
+
+	/**
+	 * Deze methode wordt gebruikt om de begin scene van Football Manager op te
+	 * laden
+	 * 
+	 * @param firstStage
+	 *            is de stage waar het start.
+	 */
+	@Override
+	public void start(Stage firstStage) {
+		try {
+
+			stage = firstStage;
+
+			firstStage.setTitle("Fishy");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(
+					"FXML/MainScreen.fxml"));
+
+			pane = (Pane) loader.load();
+
+			Scene scene = new Scene(pane);
+//			mediaPlayer = new MediaPlayer(media);
+//		    mediaPlayer.play();
+//		    mediaPlayer.setCycleCount(Integer.MAX_VALUE);
+			firstStage.setHeight(715);
+	        firstStage.setWidth(1160);
+			firstStage.setScene(scene);
+			firstStage.show();
+
+		} catch (Exception e) {
+			System.out.println("File is not found " + e);
+		}
+	}
+
+	/**
+	 * Deze methode wordt gebruikt voor het veranderen tussen scenes
+	 * 
+	 * @param bestand
+	 *            is de naam van de FXML bestand van de scene die je wilt laden.
+	 */
+	public static void switchScreen(String bestand) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Game.class.getResource(bestand));
+			stage.getScene().setRoot((Parent) loader.load());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
+
---
+++ src/main/MainScreenController.java
@@ -0,0 +1,88 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * This class contains all the event handlers of the buttons on the main screen.
+ * @author Group 6 
+ *
+ */
+public class MainScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button OptionsButton;
+
+    @FXML
+    private Button QuitButton;
+
+    @FXML
+    private Button PlayButton;
+
+    @FXML
+    void PlayEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OptionsEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void QuitEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+    	
+    	assert PlayButton != null : "fx:id=\"PlayButton\" was not injected: check your FXML file 'Main Screen.fxml'.";
+        assert OptionsButton != null : "fx:id=\"OptionsButton\" was not injected: check your FXML file 'Main Screen.fxml'.";
+        assert QuitButton != null : "fx:id=\"QuitButton\" was not injected: check your FXML file 'Main Screen.fxml'.";
+
+        
+        
+		PlayButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+
+				System.out.println("Play Clicked");
+
+			}
+		});
+		
+		OptionsButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+
+				System.out.println("Options Clicked");
+
+			}
+		});
+		
+		QuitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+
+				System.out.println("Quit Clicked");
+
+			}
+		});
+    }
+}
