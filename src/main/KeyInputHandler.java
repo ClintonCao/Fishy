ff9484f8cddf107d17241e75bff8e0d7f153eb63
Sunwei Wang <wangsunwei1996@hotmail.com>
2015-09-07 10:20:37 +0200

    working on Keyboard Input
    construct a basic structure for keyboard input handler

--- src/main/KeyInputHandler.java
+++ src/main/KeyInputHandler.java
@@ -1,7 +1,21 @@
 package main;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class KeyInputHandler extends KeyAdapter {
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		System.out.println("keyReleased="+KeyEvent.getKeyText(e.getKeyCode()));
+	}
 	
 }
