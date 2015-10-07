a038504f0d8b464753d2646f815bdb81ada8d4b4
Michieldoesburg <michiel_doesburg@hotmail.nl>
2015-10-07 17:22:01 +0200

    Added interface for Game class. Refactored code (mainly comments) in Game class. < layout, documentation, organisation

---
+++ src/interfaces/GameInterface.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import javafx.stage.Stage;
+
+public interface GameInterface {
+
+	/**
+	 * Launches the game.
+	 * 
+	 * @param firstStage -  the Stage for the start of the application.
+	 */
+	public void start(Stage firstStage);
+}
