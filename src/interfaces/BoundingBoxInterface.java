6947d1e66111662ee660478afb4827e50ee4485f
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2015-10-07 18:47:02 +0200

    Added fishbomb and enemyfish interfaces. Also refactored some code and the javadoc for these and other classes. < layout, organisation, documentation

--- src/interfaces/BoundingBoxInterface.java
+++ src/interfaces/BoundingBoxInterface.java
@@ -14,8 +14,7 @@ public interface BoundingBoxInterface {
   /**
    * Checks if two bounding boxed intersects with each other.
    * 
-   * @param other
-   *          .
+   * @param other.
    * @return true if the boxes collide.
    */
   boolean intersects(BoundingBox other);
@@ -24,8 +23,7 @@ public interface BoundingBoxInterface {
    * Update the x-coordinate of the bounding box. Used for moving bounding boxes
    * representing the fishes over the screen.
    *
-   * @param x
-   *          - the new x-coordinate.
+   * @param x - the new x-coordinate.
    */
   void updateX(int x);
 
@@ -33,16 +31,14 @@ public interface BoundingBoxInterface {
    * Update the y-coordinate of the bounding box. Used for moving bounding
    * boxes(Fish).
    * 
-   * @param y
-   *          - the new y-coordinate.
+   * @param y  - the new y-coordinate.
    */
   void updateY(int y);
 
   /**
    * Used for testing.
    * 
-   * @param other
-   *          .
+   * @param other.
    * @return true if equal.
    */
   boolean equals(Object other);
@@ -50,62 +46,42 @@ public interface BoundingBoxInterface {
   // --- Getters and setters ---
 
   /**
-   * Get the x-coordinate of the bounding box.
-   * 
    * @return the x-coordinate of the bounding box.
    */
   int getX();
 
   /**
-   * Set the y-coordinate of the bounding box.
-   * 
-   * @param x
-   *          - the new x-coordinate of the bounding box.
+   * @param x - the new x-coordinate of the bounding box.
    */
   void setX(int x);
 
   /**
-   * Get the y-coordinate of the bounding box.
-   * 
    * @return the y-coordinate of the bounding box.
    */
   int getY();
 
   /**
-   * Set the y-coordinate of the bounding box.
-   * 
-   * @param y
-   *          - the new y-coordinate of the bounding box.
+   * @param y - the new y-coordinate of the bounding box.
    */
   void setY(int y);
 
   /**
-   * Get the width of the bounding box.
-   * 
    * @return width of the bounding box.
    */
   int getWidth();
 
   /**
-   * Set the width of the bounding box.
-   * 
-   * @param width
-   *          - the new width.
+   * @param width - the new width.
    */
   void setWidth(int width);
 
   /**
-   * Get the height of the bounding box.
-   * 
    * @return height of the bounding box.
    */
   int getHeight();
 
   /**
-   * Set the height of the bounding box.
-   * 
-   * @param height
-   *          - the new height.
+   * @param height - the new height.
    */
   void setHeight(int height);
 }
