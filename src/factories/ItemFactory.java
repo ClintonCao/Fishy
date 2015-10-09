b7a22d0e82e2baadf5e11ace214cf0f03e664b41
MatthijsJWH <M.J.W.Halvemaan@student.tudelft.nl>
2015-10-09 12:57:17 +0200

    Add: Created the ItemFactory. < organisation

---
+++ src/factories/ItemFactory.java
@@ -0,0 +1,31 @@
+package factories;
+
+import main.FishBomb;
+import main.PlayerFish;
+
+public class ItemFactory {
+
+  /**
+   * This method creates an instance of an item for the player.
+   * As of this moment the only item in the game is a FishBomb,
+   * and their is no item superclass.
+   * This is why the method gives back a FishBomb instead of a generic item class.
+   * @param itemType
+   *      The item created.
+   * @param player
+   *      The player who uses the item.
+   * @return
+   *      The instance of the item created.
+   */
+  public FishBomb createItem(String itemType, PlayerFish player) {
+    
+    switch (itemType.toUpperCase()) {
+      case "FISHBOMB": 
+        return FishBomb.createFishBomb(player);
+      default:
+        return null;
+    }
+  
+  }
+  
+}
