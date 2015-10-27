2da1b4cc56955d7424e0c5498c7ee8205f6237d4
Sunwei Wang <wangsunwei1996@hotmail.com>
2015-10-27 11:10:44 +0100

    Entity class now has 100% test coverage < testing

--- src/test/java/nl/tudelft/fishy/EnemyFishTest.java
+++ src/test/java/nl/tudelft/fishy/EnemyFishTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import javafx.scene.image.Image;
 
 import org.junit.Test;
 
@@ -56,4 +57,36 @@ public class EnemyFishTest {
     assertTrue(enemyFish.isLefty());
   }
 
+  /**
+   * Test the method of setSprite.
+   */
+  @Test
+  public void testSetSprite() { 
+    Sprite sprite = mock(Sprite.class);
+    int movespeed = 2;
+    boolean isLefty = true;
+    EnemyFish enemyFish = new EnemyFish(movespeed, isLefty, sprite);
+    Sprite sprite1 = mock(Sprite.class);
+    enemyFish.setSprite(sprite1);
+    assertEquals(sprite1,enemyFish.getSprite());
+  }
+  
+  /**
+   * Test whether two entities can intersect with each other.
+   */
+  @Test
+  public void testIntersect() {
+    BoundingBox boundingBox = new BoundingBox(53, 129, 67, 2);
+    BoundingBox aabb = boundingBox;
+    Image image = mock(Image.class);
+    Sprite sprite = new Sprite(image, boundingBox);
+    Sprite sprite1 = new Sprite(image, aabb);
+    int movespeed = 2;
+    boolean isLefty = true;
+    EnemyFish enemyFish = new EnemyFish(movespeed, isLefty, sprite); 
+    int movespeed1 = 3;
+    boolean isLefty1 = false;
+    EnemyFish enemyFish1 =  new EnemyFish(movespeed1, isLefty1, sprite1);
+    assertTrue(enemyFish.intersects(enemyFish1));
+  }
 }
